0c485413b03ea99e42dfd3a835b3b32621ef04b9
gaeun <geun5744@gmail.com>
2023-11-19 16:26:45 +0900

    [ADD] Timber 추가

--- app/src/main/java/com/growthook/aos/App.kt
+++ app/src/main/java/com/growthook/aos/App.kt
@@ -3,11 +3,13 @@ package com.growthook.aos
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, "{NATIVE_APP_KEY}")
+        Timber.plant(Timber.DebugTree())
     }
 }
