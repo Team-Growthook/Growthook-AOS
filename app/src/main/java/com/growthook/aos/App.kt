0d0f04c615f6ae839cf33fa63121e1ed7c25fa43
gaeun <geun5744@gmail.com>
2023-11-19 15:56:01 +0900

    [CHORE/#3] 카카오 로그인 기초 셋팅

--- app/src/main/java/com/growthook/aos/App.kt
+++ app/src/main/java/com/growthook/aos/App.kt
@@ -1,11 +1,13 @@
 package com.growthook.aos
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        KakaoSdk.init(this, "{NATIVE_APP_KEY}")
     }
 }
