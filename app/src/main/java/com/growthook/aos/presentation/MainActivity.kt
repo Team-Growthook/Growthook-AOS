cdc9cfe17c31635f38de12e679de131998ec5507
gaeun <geun5744@gmail.com>
2023-11-20 22:32:35 +0900

    [FIX/#4] R.layout.activity_main -> binding.root로 수정

--- app/src/main/java/com/growthook/aos/presentation/MainActivity.kt
+++ app/src/main/java/com/growthook/aos/presentation/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         initBottomNavigation()
     }
 
