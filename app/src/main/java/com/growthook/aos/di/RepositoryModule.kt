a1049a0bb19cff2212859a3dacec8f1e1807b001
gaeun <geun5744@gmail.com>
2023-11-20 14:07:57 +0900

    [FEAT/#4] RepositoryModule 구현

---
+++ app/src/main/java/com/growthook/aos/di/RepositoryModule.kt
@@ -0,0 +1,22 @@
+package com.growthook.aos.di
+
+import com.growthook.aos.data.repository.TokenRepositoryImpl
+import com.growthook.aos.domain.repository.TokenRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Singleton
+    @Binds
+    abstract fun providesTokenRepository(repoImpl: TokenRepositoryImpl): TokenRepository
+
+    @Singleton
+    @Binds
+    abstract fun providesUserRepository(repoImpl: TokenRepositoryImpl): TokenRepository
+}
