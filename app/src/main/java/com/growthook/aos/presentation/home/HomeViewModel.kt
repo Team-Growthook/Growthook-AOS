5e4a2fa4abac3f9123d8892aec7b4fe78939a823
gaeun <geun5744@gmail.com>
2023-11-26 18:52:05 +0900

    [ADD/#2] 임시 HomeViewModel 추가

---
+++ app/src/main/java/com/growthook/aos/presentation/home/HomeViewModel.kt
@@ -0,0 +1,117 @@
+package com.growthook.aos.presentation.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.growthook.aos.domain.entity.Cave
+import com.growthook.aos.domain.entity.Insight
+import com.growthook.aos.domain.usecase.local.GetUserUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val getUserUseCase: GetUserUseCase,
+) : ViewModel() {
+
+    private val _nickName = MutableLiveData<String>()
+    val nickname: LiveData<String> = _nickName
+
+    private val _insightAmount = MutableLiveData(0)
+    val insightAmount: LiveData<Int> = _insightAmount
+
+    init {
+        viewModelScope.launch {
+            getUserUseCase.invoke().name.let { nickName ->
+                _nickName.value = nickName
+            }
+        }
+    }
+
+    var dummyInsights = listOf(
+        Insight(
+            "제목1",
+            12,
+            isScraped = true,
+            isLocked = false,
+            isAction = false,
+        ),
+        Insight(
+            "제목2",
+            24,
+            isScraped = false,
+            isLocked = false,
+            isAction = false,
+        ),
+        Insight(
+            "제목3",
+            12,
+            isScraped = true,
+            isLocked = true,
+            isAction = false,
+        ),
+        Insight(
+            "제목4",
+            12,
+            isScraped = true,
+            isLocked = false,
+            isAction = false,
+        ),
+        Insight(
+            "제목5",
+            12,
+            isScraped = false,
+            isLocked = true,
+            isAction = false,
+        ),
+        Insight(
+            "제목6",
+            12,
+            isScraped = true,
+            isLocked = false,
+            isAction = false,
+        ),
+        Insight(
+            "제목7",
+            12,
+            isScraped = false,
+            isLocked = false,
+            isAction = true,
+        ),
+        Insight(
+            "제목8",
+            12,
+            isScraped = true,
+            isLocked = false,
+            isAction = false,
+        ),
+        Insight(
+            "제목9",
+            12,
+            isScraped = true,
+            isLocked = true,
+            isAction = false,
+        ),
+        Insight(
+            "제목10",
+            12,
+            isScraped = false,
+            isLocked = false,
+            isAction = true,
+        ),
+
+    )
+
+    val dummyCave = listOf(
+        Cave("연습용"),
+        Cave("연습연습연습연습"),
+        Cave("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ"),
+        Cave("동굴"),
+        Cave("연습용"),
+        Cave("연습연습연습연습"),
+        Cave("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ"),
+        Cave("동굴"),
+    )
+}
