b38649dcd65dd50df609fa1217ec0d5fe8a4619c
gaeun <geun5744@gmail.com>
2023-11-26 18:52:44 +0900

    [FEAT/#2] adapter 및 title 초기화 구현

--- app/src/main/java/com/growthook/aos/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/growthook/aos/presentation/home/HomeFragment.kt
@@ -5,57 +5,55 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.growthook.aos.R
+import androidx.fragment.app.viewModels
+import com.growthook.aos.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding
+        get() = requireNotNull(_binding) { "binding is null" }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initTitleText()
+        initAdapter()
+    }
+
+    private fun initTitleText() {
+        viewModel.nickname.observe(viewLifecycleOwner) { nickName ->
+            binding.tvHomeAppbarTitle.text = "${nickName}님의 동굴 속"
+        }
+        viewModel.insightAmount.observe(viewLifecycleOwner) { insightAmount ->
+            binding.tvHomeInsightTitle.text = "${insightAmount}개의 씨앗을 모았어요!"
+        }
+    }
+
+    private fun initAdapter() {
+        val homeInsightAdapter = HomeInsightAdapter()
+        homeInsightAdapter.submitList(viewModel.dummyInsights)
+        binding.rvHomeInsight.adapter = homeInsightAdapter
+
+        val caveAdapter = CaveAdapter()
+        caveAdapter.submitList(viewModel.dummyCave)
+        binding.rvHomeCave.adapter = caveAdapter
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
