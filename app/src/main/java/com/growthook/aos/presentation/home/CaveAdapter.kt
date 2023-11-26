3296e8d534871e6011ba7a1730747669c149f2c7
gaeun <geun5744@gmail.com>
2023-11-26 18:50:46 +0900

    [FEAT/#2] CaveAdapter 구현

---
+++ app/src/main/java/com/growthook/aos/presentation/home/CaveAdapter.kt
@@ -0,0 +1,47 @@
+package com.growthook.aos.presentation.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.growthook.aos.databinding.ItemHomeCaveBinding
+import com.growthook.aos.domain.entity.Cave
+import com.growthook.aos.util.ItemDiffCallback
+
+class CaveAdapter : ListAdapter<Cave, CaveAdapter.CaveViewHolder>(diffCallback) {
+
+    private var listener: OnItemClickListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CaveViewHolder {
+        val binding: ItemHomeCaveBinding =
+            ItemHomeCaveBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CaveViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CaveViewHolder, position: Int) {
+        return holder.onBind(currentList[position])
+    }
+
+    class CaveViewHolder(private val binding: ItemHomeCaveBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun onBind(item: Cave) {
+            binding.tvHomeCaveTitle.text = item.title
+        }
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(item: Cave)
+    }
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
+    }
+
+    companion object {
+        private val diffCallback =
+            ItemDiffCallback<Cave>(
+                onContentsTheSame = { old, new -> old == new },
+                onItemsTheSame = { old, new -> old == new },
+            )
+    }
+}
