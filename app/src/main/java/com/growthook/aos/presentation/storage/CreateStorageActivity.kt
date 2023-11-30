e9940bc4e38daf53db46b110f4a1828bc06c5d6e
KIM MINJEONG <minjeong030420@gmail.com>
2023-11-30 20:18:12 +0900

    [FEAT/#12] hideKeyboard 추가

--- app/src/main/java/com/growthook/aos/presentation/storage/CreateStorageActivity.kt
+++ app/src/main/java/com/growthook/aos/presentation/storage/CreateStorageActivity.kt
@@ -1,4 +1,32 @@
 package com.growthook.aos.presentation.storage
 
-class CreateStorageActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import com.growthook.aos.R
+import com.growthook.aos.databinding.ActivityCreateStorageBinding
+import com.growthook.aos.util.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CreateStorageActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityCreateStorageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCreateStorageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        hideKeyboard()
+        return super.dispatchTouchEvent(ev)
+    }
+
+    private fun hideKeyboard() {
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+    }
 }
