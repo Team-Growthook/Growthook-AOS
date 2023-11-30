997fb6c796a99826ca1233ce87e000b536e235e9
KIM MINJEONG <minjeong030420@gmail.com>
2023-11-30 21:33:10 +0900

    [UI/#12] 보관함 생성뷰 UI 작업

--- app/src/main/java/com/growthook/aos/presentation/storage/CreateStorageActivity.kt
+++ app/src/main/java/com/growthook/aos/presentation/storage/CreateStorageActivity.kt
@@ -1,23 +1,26 @@
 package com.growthook.aos.presentation.storage
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
-import com.growthook.aos.R
 import com.growthook.aos.databinding.ActivityCreateStorageBinding
-import com.growthook.aos.util.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class CreateStorageActivity: AppCompatActivity() {
+class CreateStorageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCreateStorageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCreateStorageBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initMakeCreateStorageView()
+    }
+
+    private fun initMakeCreateStorageView() {
+        changeTextLayout()
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
@@ -28,5 +31,9 @@ class CreateStorageActivity: AppCompatActivity() {
     private fun hideKeyboard() {
         val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+    }
+
+    private fun changeTextLayout() {
+        //
     }
 }
