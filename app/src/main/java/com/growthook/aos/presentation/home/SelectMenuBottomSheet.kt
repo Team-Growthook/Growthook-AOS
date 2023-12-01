648f3d3bb634f02c91e10f3e9e9ec2c3cb4ae693
gaeun <geun5744@gmail.com>
2023-12-01 17:18:21 +0900

    [FEAT/#2] 뒤로가기 버튼 -> onDisMiss 이벤트 감지로 리팩토링 및 viewModel 데이터 전달 구현

--- app/src/main/java/com/growthook/aos/presentation/home/SelectMenuBottomSheet.kt
+++ app/src/main/java/com/growthook/aos/presentation/home/SelectMenuBottomSheet.kt
@@ -1,26 +1,22 @@
 package com.growthook.aos.presentation.home
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.Toast
-import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.activityViewModels
 import com.growthook.aos.R
 import com.growthook.aos.databinding.FragmentSelectMenuBottomsheetBinding
 import com.growthook.aos.util.base.BaseBottomSheetFragment
+import timber.log.Timber
 
-class SelectMenuBottomSheet :
+class SelectMenuBottomSheet() :
     BaseBottomSheetFragment<FragmentSelectMenuBottomsheetBinding>(R.layout.fragment_select_menu_bottomsheet) {
 
-    private val callback = object : OnBackPressedCallback(true) {
-        override fun handleOnBackPressed() {
-            Toast.makeText(context, "뒤로가기", Toast.LENGTH_SHORT).show()
-
-            isEnabled = false
-        }
-    }
+    private val viewModel: HomeViewModel by activityViewModels()
 
     override fun getFragmentBinding(
         inflater: LayoutInflater,
@@ -36,7 +32,12 @@ class SelectMenuBottomSheet :
                 dialog.window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
             }
         }
+    }
 
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+        Toast.makeText(requireContext(), "바텀시트 내려감", Toast.LENGTH_SHORT)
+        Timber.d("바텀시트 내려감 on fragment")
+        viewModel.isMenuDismissed.value = true
     }
 }
