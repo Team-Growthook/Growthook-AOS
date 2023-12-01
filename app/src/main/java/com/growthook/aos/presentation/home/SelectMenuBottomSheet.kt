08392c9df41fe78744994fb54b667de71e8c551a
gaeun <geun5744@gmail.com>
2023-12-01 15:05:28 +0900

    [FEAT/#2] 뒤로가기 버튼 클릭시 toast 메시지 임시 기능 구현

--- app/src/main/java/com/growthook/aos/presentation/home/SelectMenuBottomSheet.kt
+++ app/src/main/java/com/growthook/aos/presentation/home/SelectMenuBottomSheet.kt
@@ -5,12 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import com.growthook.aos.R
 import com.growthook.aos.databinding.FragmentSelectMenuBottomsheetBinding
 import com.growthook.aos.util.base.BaseBottomSheetFragment
 
 class SelectMenuBottomSheet :
     BaseBottomSheetFragment<FragmentSelectMenuBottomsheetBinding>(R.layout.fragment_select_menu_bottomsheet) {
+
+    private val callback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            Toast.makeText(context, "뒤로가기", Toast.LENGTH_SHORT).show()
+
+            isEnabled = false
+        }
+    }
+
     override fun getFragmentBinding(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,5 +36,7 @@ class SelectMenuBottomSheet :
                 dialog.window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
             }
         }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
     }
 }
