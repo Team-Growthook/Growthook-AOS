2b4619e849a601b403219efd44177b3b26c542f9
gaeun <geun5744@gmail.com>
2023-12-03 21:00:48 +0900

    [DELETE/#2] parcelize 제거

--- app/src/main/java/com/growthook/aos/domain/entity/Insight.kt
+++ app/src/main/java/com/growthook/aos/domain/entity/Insight.kt
@@ -1,14 +1,10 @@
 package com.growthook.aos.domain.entity
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class Insight(
     val title: String,
     val remainedLock: Int,
     val isScraped: Boolean,
     val isLocked: Boolean,
     val isAction: Boolean,
-    val insightId: Int
-) : Parcelable
+    val insightId: Int,
+)
