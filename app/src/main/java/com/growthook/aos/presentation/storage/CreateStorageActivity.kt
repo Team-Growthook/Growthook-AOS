247f2df7c03fda10cd16380de0f9440d8613f5b3
KIM MINJEONG <minjeong030420@gmail.com>
2023-12-04 19:45:28 +0900

    [FEAT/#12] 조건 만족 시 버튼 활성화 구현

--- app/src/main/java/com/growthook/aos/presentation/storage/CreateStorageActivity.kt
+++ app/src/main/java/com/growthook/aos/presentation/storage/CreateStorageActivity.kt
@@ -1,29 +1,34 @@
 package com.growthook.aos.presentation.storage
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.growthook.aos.R
 import com.growthook.aos.databinding.ActivityCreateStorageBinding
-import com.growthook.aos.util.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CreateStorageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCreateStorageBinding
+    private val viewModel by viewModels<CreateStorageViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCreateStorageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initMakeCreateStorageView()
+        initMakeStorageView()
+        initGetStorageContent()
     }
 
-    private fun initMakeCreateStorageView() {
-        changeTextLayout()
+    private fun initMakeStorageView() {
+        clickOpenSwitchBtn()
+        setBtnEnabled()
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
@@ -40,7 +45,57 @@ class CreateStorageActivity : AppCompatActivity() {
         }
     }
 
-    private fun changeTextLayout() {
-        //
+    private fun clickOpenSwitchBtn() {
+        binding.switchOpen.setOnCheckedChangeListener{ CompoundButton, onSwitch ->
+            if (onSwitch) {
+                //
+            } else {
+                //
+            }
+        }
+    }
+
+    private fun initGetStorageContent() {
+        with (binding) {
+            edtStorageName.addTextChangedListener(object : TextWatcher{
+                override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    viewModel.getStorageName(s.toString())
+                }
+            })
+
+            edtStorageIntroduction.addTextChangedListener(object : TextWatcher{
+                override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    viewModel.getStorageIntroduction(s.toString())
+                }
+            })
+        }
+    }
+
+    private fun setBtnEnabled() {
+        viewModel.checkBtnEnabled.observe(this) {
+            if (it) {
+                with (binding) {
+                    btnStorageCreate.isEnabled = true
+                    btnStorageCreate.setBackgroundResource(R.drawable.shape_storage_btn_active)
+                }
+            } else {
+                with (binding) {
+                    btnStorageCreate.isEnabled = false
+                    btnStorageCreate.setBackgroundResource(R.drawable.shape_storage_btn_inactive)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/growthook/aos/presentation/storage/CreateStorageViewModel.kt
+++ app/src/main/java/com/growthook/aos/presentation/storage/CreateStorageViewModel.kt
@@ -1,6 +1,36 @@
 package com.growthook.aos.presentation.storage
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class CreateStorageViewModel: ViewModel() {
+@HiltViewModel
+class CreateStorageViewModel @Inject constructor() : ViewModel() {
+
+    private val _storageName = MutableLiveData<String>()
+    val storageName: LiveData<String>
+        get() = _storageName
+
+    private val _storageIntroduction = MutableLiveData<String>()
+    val storageIntroduction: LiveData<String>
+        get() = _storageIntroduction
+
+    fun getStorageName(name: String) {
+        _storageName.value = name
+    }
+
+    fun getStorageIntroduction(introduction: String) {
+        _storageIntroduction.value = introduction
+    }
+
+    val checkBtnEnabled = MediatorLiveData<Boolean>().apply {
+        addSource(storageName) { value = checkCreateStorageEnabled() }
+        addSource(storageIntroduction) { value = checkCreateStorageEnabled() }
+    }
+
+    private fun checkCreateStorageEnabled() : Boolean =
+        !storageName.value.isNullOrBlank() && !storageIntroduction.value.isNullOrBlank()
 }
