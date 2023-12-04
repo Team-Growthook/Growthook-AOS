dda112794c49e571c800285456930cb5969835a3
Cat-JiYoung <kimyamae82@gmail.com>
2023-12-05 02:31:14 +0900

    [FEAT/#17] 인사이트 내용 접고 펴는 로직 구현

--- app/src/main/java/com/growthook/aos/presentation/insight/actionplan/ActionplanInsightActivity.kt
+++ app/src/main/java/com/growthook/aos/presentation/insight/actionplan/ActionplanInsightActivity.kt
@@ -1,6 +1,7 @@
 package com.growthook.aos.presentation.insight.actionplan
 
 import android.os.Bundle
+import android.view.View
 import com.growthook.aos.databinding.ActivityActionplanInsightBinding
 import com.growthook.aos.util.base.BaseActivity
 
@@ -8,5 +9,17 @@ class ActionplanInsightActivity :
     BaseActivity<ActivityActionplanInsightBinding>({ ActivityActionplanInsightBinding.inflate(it) }) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        foldInsightContent()
+    }
+
+    private fun foldInsightContent() {
+        binding.clActionInsightCollapse.setOnClickListener {
+            binding.clActionInsightCollapse.visibility = View.GONE
+            binding.clActionInsightExpanded.visibility = View.VISIBLE
+        }
+        binding.clActionInsightExpanded.setOnClickListener {
+            binding.clActionInsightExpanded.visibility = View.GONE
+            binding.clActionInsightCollapse.visibility = View.VISIBLE
+        }
     }
 }
