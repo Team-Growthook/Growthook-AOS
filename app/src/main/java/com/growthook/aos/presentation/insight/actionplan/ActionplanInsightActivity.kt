f383e2248da9f477829ce636b73734103f9f1110
Cat-JiYoung <kimyamae82@gmail.com>
2023-12-05 01:38:59 +0900

    [FEAT/#17] 액션플랜 생성 접힌 뷰 xml 구현

---
+++ app/src/main/java/com/growthook/aos/presentation/insight/actionplan/ActionplanInsightActivity.kt
@@ -0,0 +1,12 @@
+package com.growthook.aos.presentation.insight.actionplan
+
+import android.os.Bundle
+import com.growthook.aos.databinding.ActivityActionplanInsightBinding
+import com.growthook.aos.util.base.BaseActivity
+
+class ActionplanInsightActivity :
+    BaseActivity<ActivityActionplanInsightBinding>({ ActivityActionplanInsightBinding.inflate(it) }) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
