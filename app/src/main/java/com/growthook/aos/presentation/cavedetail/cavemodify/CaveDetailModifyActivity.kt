dd1d11fb94b3267aca94cc9bc5460f32abefd6a8
gaeun <geun5744@gmail.com>
2023-12-06 21:26:13 +0900

    [FEAT/#16] 보관함 편집시 완료 버튼 활성화 구현

--- app/src/main/java/com/growthook/aos/presentation/cavedetail/cavemodify/CaveDetailModifyActivity.kt
+++ app/src/main/java/com/growthook/aos/presentation/cavedetail/cavemodify/CaveDetailModifyActivity.kt
@@ -1,15 +1,46 @@
 package com.growthook.aos.presentation.cavedetail.cavemodify
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.activity.viewModels
 import com.growthook.aos.databinding.ActivityCaveDetailModifyBinding
 import com.growthook.aos.util.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
+@AndroidEntryPoint
 class CaveDetailModifyActivity :
     BaseActivity<ActivityCaveDetailModifyBinding>({ ActivityCaveDetailModifyBinding.inflate(it) }) {
+    private val viewModel by viewModels<CaveDetailModifyViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Timber.d("동굴 수정 ${intent.getStringExtra("caveName")}")
+        observeIsModified()
+        canClickFinishBtn()
+    }
+
+    private fun observeIsModified() {
+        val textWatcher: TextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.setIsModified(true)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        }
+
+        binding.edtCaveModifyName.addTextChangedListener(textWatcher)
+        binding.edtCaveModifyDesc.addTextChangedListener(textWatcher)
+    }
+
+    private fun canClickFinishBtn() {
+        viewModel.isCaveModified.observe(this) { isModified ->
+            binding.btnCaveModify.isEnabled = isModified
+        }
     }
 }
