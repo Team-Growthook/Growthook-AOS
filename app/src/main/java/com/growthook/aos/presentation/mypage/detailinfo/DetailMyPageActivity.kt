0ac27bef4a9ea2422575966c07d88b92b51507f6
gaeun <geun5744@gmail.com>
2023-12-10 22:33:04 +0900

    [UI/#21] 마이페이지 상세조회 UI 구현

---
+++ app/src/main/java/com/growthook/aos/presentation/mypage/detailinfo/DetailMyPageActivity.kt
@@ -0,0 +1,13 @@
+package com.growthook.aos.presentation.mypage.detailinfo
+
+import android.os.Bundle
+import com.growthook.aos.databinding.ActivityDetailMyPageBinding
+import com.growthook.aos.util.base.BaseActivity
+
+class DetailMyPageActivity :
+    BaseActivity<ActivityDetailMyPageBinding>({ ActivityDetailMyPageBinding.inflate(it) }) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
