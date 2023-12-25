a642e00b3b063e541eec30e9c03af7d790513f24
KIM MINJEONG <minjeong030420@gmail.com>
2023-12-26 05:39:52 +0900

    [ADD/#23] edt clearFocus 추가

--- app/src/main/java/com/growthook/aos/presentation/insight/write/InsightWriteActivity.kt
+++ app/src/main/java/com/growthook/aos/presentation/insight/write/InsightWriteActivity.kt
@@ -1,9 +1,11 @@
 package com.growthook.aos.presentation.insight.write
 
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.activity.viewModels
 import com.growthook.aos.databinding.ActivityInsightWriteBinding
 import com.growthook.aos.util.base.BaseActivity
+import com.growthook.aos.util.extension.hideKeyboard
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,5 +19,19 @@ class InsightWriteActivity: BaseActivity<ActivityInsightWriteBinding>({
         super.onCreate(savedInstanceState)
         binding = ActivityInsightWriteBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        this.currentFocus?.let { hideKeyboard(it) }
+        with(binding) {
+            edtInsightWriteInsight.clearFocus()
+            edtInsightWriteMemo.clearFocus()
+            edtInsightWriteCave.clearFocus()
+            edtInsightWriteUrl.clearFocus()
+            edtInsightWriteUrlChoice.clearFocus()
+            edtInsightWriteGoal.clearFocus()
+        }
+
+        return super.dispatchTouchEvent(ev)
     }
 }
