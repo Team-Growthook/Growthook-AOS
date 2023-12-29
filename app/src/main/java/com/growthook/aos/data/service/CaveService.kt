0df63c6ec644bc5423a4b89faeab00d2498f8582
gaeun <geun5744@gmail.com>
2023-12-30 00:45:28 +0900

    [ADD] 동굴 리스트 get service 추가

--- app/src/main/java/com/growthook/aos/data/service/CaveService.kt
+++ app/src/main/java/com/growthook/aos/data/service/CaveService.kt
@@ -1,7 +1,9 @@
 package com.growthook.aos.data.service
 
 import com.growthook.aos.data.model.response.BaseResponse
+import com.growthook.aos.data.model.response.ResponseGetCavesDto
 import retrofit2.http.DELETE
+import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface CaveService {
@@ -10,4 +12,9 @@ interface CaveService {
     suspend fun deleteCave(
         @Path("caveId") caveId: Int,
     ): BaseResponse<Unit>
+
+    @GET("api/v1/member/{memberId}/cave/all")
+    suspend fun getCaves(
+        @Path("memberId") memberId: Int,
+    ): BaseResponse<List<ResponseGetCavesDto>>
 }
