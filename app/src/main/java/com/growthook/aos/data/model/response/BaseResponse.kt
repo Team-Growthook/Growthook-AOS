0760d86a78bd05234ebfb33aeb4bfcf889de0f7c
gaeun <geun5744@gmail.com>
2023-12-29 18:17:39 +0900

    [ADD] BaseResponse success 추가

---
+++ app/src/main/java/com/growthook/aos/data/model/response/BaseResponse.kt
@@ -0,0 +1,11 @@
+package com.growthook.aos.data.model.response
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BaseResponse<T>(
+    val status: Int,
+    val success: Boolean,
+    val message: String,
+    val data: T? = null,
+)
