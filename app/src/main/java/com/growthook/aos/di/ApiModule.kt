de54811d9f918bbfe9667073e09c48880e070a66
gaeun <geun5744@gmail.com>
2023-12-29 23:11:03 +0900

    [FIX] Annotation 수정

--- app/src/main/java/com/growthook/aos/di/ApiModule.kt
+++ app/src/main/java/com/growthook/aos/di/ApiModule.kt
@@ -3,11 +3,13 @@ package com.growthook.aos.di
 import com.growthook.aos.data.service.CaveService
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Module
-@Singleton
+@InstallIn(SingletonComponent::class)
 object ApiModule {
 
     @Provides
