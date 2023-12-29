eb2aa525840b9e17eedb92c617cdd12d89807f31
KIM MINJEONG <minjeong030420@gmail.com>
2023-12-30 03:39:27 +0900

    [UI/#35] 목표 기간 수정 시도 시 토스트 띄우기

--- app/src/main/java/com/growthook/aos/presentation/insight/noactionplan/SeedModifyActivity.kt
+++ app/src/main/java/com/growthook/aos/presentation/insight/noactionplan/SeedModifyActivity.kt
@@ -1,6 +1,8 @@
 package com.growthook.aos.presentation.insight.noactionplan
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import com.growthook.aos.databinding.ActivitySeedModifyBinding
 import com.growthook.aos.util.base.BaseActivity
 
@@ -8,9 +10,19 @@ class SeedModifyActivity : BaseActivity<ActivitySeedModifyBinding>({
     ActivitySeedModifyBinding.inflate(it)
 }) {
 
+    private val viewModel by viewModels<SeedModifyViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySeedModifyBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initSetClickGoalMonth()
+    }
+
+    private fun initSetClickGoalMonth() {
+        binding.layoutSeedModifyGoal.setOnClickListener {
+            Toast.makeText(this, "목표 기간은 수정할 수 없어요", Toast.LENGTH_SHORT).show()
+        }
     }
 }
