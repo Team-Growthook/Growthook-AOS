0d2e58023423df5acf01b537f91c8cf3ba1ba940
gaeun <geun5744@gmail.com>
2023-12-30 00:59:25 +0900

    [ADD] 동굴리스트 get repository에 추가

--- app/src/main/java/com/growthook/aos/data/repository/CaveRepositoryImpl.kt
+++ app/src/main/java/com/growthook/aos/data/repository/CaveRepositoryImpl.kt
@@ -1,11 +1,17 @@
 package com.growthook.aos.data.repository
 
 import com.growthook.aos.data.datasource.remote.CaveDataSource
+import com.growthook.aos.domain.entity.Cave
 import com.growthook.aos.domain.repository.CaveRepository
 import javax.inject.Inject
 
-class CaveRepositoryImpl @Inject constructor(private val caveDataSource: CaveDataSource) :CaveRepository {
-    override suspend fun deleteCave(caveId: Int): Result<Unit> = kotlin.runCatching {
+class CaveRepositoryImpl @Inject constructor(private val caveDataSource: CaveDataSource) :
+    CaveRepository {
+    override suspend fun deleteCave(caveId: Int): Result<Unit> = runCatching {
         caveDataSource.deleteCave(caveId)
+    }
+
+    override suspend fun getCaves(memberId: Int): Result<List<Cave>> = runCatching {
+        caveDataSource.getCaves(memberId).data.map { cave -> cave.toCave() }
     }
 }
--- app/src/main/java/com/growthook/aos/domain/repository/CaveRepository.kt
+++ app/src/main/java/com/growthook/aos/domain/repository/CaveRepository.kt
@@ -1,6 +1,10 @@
 package com.growthook.aos.domain.repository
 
+import com.growthook.aos.domain.entity.Cave
+
 interface CaveRepository {
 
     suspend fun deleteCave(caveId: Int): Result<Unit>
+
+    suspend fun getCaves(memberId: Int): Result<List<Cave>>
 }
