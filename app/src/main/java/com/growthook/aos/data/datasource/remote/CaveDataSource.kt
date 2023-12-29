42e56f86dfcdc78fd85c3f484eedbccc00c3cf05
gaeun <geun5744@gmail.com>
2023-12-30 00:47:38 +0900

    [ADD] 동굴 리스트 get dataSource에 추가

--- app/src/main/java/com/growthook/aos/data/datasource/remote/CaveDataSource.kt
+++ app/src/main/java/com/growthook/aos/data/datasource/remote/CaveDataSource.kt
@@ -1,7 +1,10 @@
 package com.growthook.aos.data.datasource.remote
 
 import com.growthook.aos.data.model.response.BaseResponse
+import com.growthook.aos.data.model.response.ResponseGetCavesDto
 
 interface CaveDataSource {
     suspend fun deleteCave(caveId: Int): BaseResponse<Unit>
+
+    suspend fun getCaves(memberId: Int): BaseResponse<List<ResponseGetCavesDto>>
 }
--- app/src/main/java/com/growthook/aos/data/datasource/remote/impl/CaveDataSourceImpl.kt
+++ app/src/main/java/com/growthook/aos/data/datasource/remote/impl/CaveDataSourceImpl.kt
@@ -2,9 +2,11 @@ package com.growthook.aos.data.datasource.remote.impl
 
 import com.growthook.aos.data.datasource.remote.CaveDataSource
 import com.growthook.aos.data.model.response.BaseResponse
+import com.growthook.aos.data.model.response.ResponseGetCavesDto
 import com.growthook.aos.data.service.CaveService
 import javax.inject.Inject
 
 class CaveDataSourceImpl @Inject constructor(private val apiService: CaveService) : CaveDataSource {
     override suspend fun deleteCave(caveId: Int): BaseResponse<Unit> = apiService.deleteCave(caveId)
+    override suspend fun getCaves(memberId: Int): BaseResponse<List<ResponseGetCavesDto>> = apiService.getCaves(memberId)
 }
