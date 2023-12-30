5055af862b41426e90f26a5f1658d2308db3fa21
KIM MINJEONG <minjeong030420@gmail.com>
2023-12-31 02:02:57 +0900

    [FEAT/#35] edt 값 받아서 버튼 활성화 로직 생성

--- app/src/main/java/com/growthook/aos/presentation/insight/noactionplan/SeedModifyActivity.kt
+++ app/src/main/java/com/growthook/aos/presentation/insight/noactionplan/SeedModifyActivity.kt
@@ -1,11 +1,16 @@
 package com.growthook.aos.presentation.insight.noactionplan
 
 import android.os.Bundle
+import android.view.MotionEvent
 import android.widget.Toast
 import androidx.activity.viewModels
 import com.growthook.aos.databinding.ActivitySeedModifyBinding
 import com.growthook.aos.util.base.BaseActivity
+import com.growthook.aos.util.extension.CommonTextWatcher
+import com.growthook.aos.util.extension.hideKeyboard
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SeedModifyActivity : BaseActivity<ActivitySeedModifyBinding>({
     ActivitySeedModifyBinding.inflate(it)
 }) {
@@ -18,7 +23,20 @@ class SeedModifyActivity : BaseActivity<ActivitySeedModifyBinding>({
         setContentView(binding.root)
 
         initSetBeforeModifyInfo()
+        initGetSeedModifyEdt()
         initSetClickGoalMonth()
+        initSetBtnEnabled()
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        this.currentFocus?.let { hideKeyboard(it) }
+        with (binding) {
+            edtSeedModifyInsight.clearFocus()
+            edtSeedModifyMemo.clearFocus()
+            edtSeedModifySource.clearFocus()
+            edtSeedModifyUrl.clearFocus()
+        }
+        return super.dispatchTouchEvent(ev)
     }
 
     private fun initSetBeforeModifyInfo() {
@@ -28,8 +46,8 @@ class SeedModifyActivity : BaseActivity<ActivitySeedModifyBinding>({
             with (binding) {
                 edtSeedModifyInsight.setText(seedInfo.insight)
                 edtSeedModifyMemo.setText(seedInfo.memo)
-                edtSeedModifyUrl.setText(seedInfo.source)
-                edtSeedModifyUrlChoice.setText(seedInfo.url)
+                edtSeedModifySource.setText(seedInfo.source)
+                edtSeedModifyUrl.setText(seedInfo.url)
                 tvSeedModifyGoalSelected.text = "${seedInfo.goalMonth}개월"
                 tvSeedModifyCaveSelected.text = seedInfo.cave
             }
@@ -40,9 +58,48 @@ class SeedModifyActivity : BaseActivity<ActivitySeedModifyBinding>({
         // TODO 인사이트 조회 뷰에서 넘어온 cave 정보 받기
     }
 
+    private fun initGetSeedModifyEdt() {
+        val insightModifyTextWatcher = CommonTextWatcher( afterChanged = { edtInsightModify ->
+            viewModel.setInsightModify(edtInsightModify.toString())
+        })
+        val memoModifyTextWatcher = CommonTextWatcher( afterChanged = { edtMemoModify ->
+            viewModel.setInsightModify(edtMemoModify.toString())
+        })
+        val sourceModifyTextWatcher = CommonTextWatcher( afterChanged = { edtSourceModify ->
+            viewModel.setSourceModify(edtSourceModify.toString())
+        })
+        val urlModifyTextWatcher = CommonTextWatcher( afterChanged = { edtUrlModify ->
+            viewModel.setUrlModify(edtUrlModify.toString())
+        })
+
+        with (binding) {
+            edtSeedModifyInsight.addTextChangedListener(insightModifyTextWatcher)
+            edtSeedModifyMemo.addTextChangedListener(memoModifyTextWatcher)
+            edtSeedModifySource.addTextChangedListener(sourceModifyTextWatcher)
+            edtSeedModifyUrl.addTextChangedListener(urlModifyTextWatcher)
+        }
+    }
+
     private fun initSetClickGoalMonth() {
         binding.layoutSeedModifyGoal.setOnClickListener {
             Toast.makeText(this, "목표 기간은 수정할 수 없어요", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun initSetBtnEnabled() {
+        viewModel.checkSeedModifyBtnEnabled.observe(this) {
+            with (binding.btnSeedModify) {
+                if (it) {
+                    isEnabled = true
+                    clickSeedModifyBtn()
+                } else {
+                    isEnabled = false
+                }
+            }
+        }
+    }
+
+    private fun clickSeedModifyBtn() {
+        // TODO 씨앗 수정 완료 버튼 클릭 시 로직
     }
 }
--- app/src/main/java/com/growthook/aos/presentation/insight/noactionplan/SeedModifyViewModel.kt
+++ app/src/main/java/com/growthook/aos/presentation/insight/noactionplan/SeedModifyViewModel.kt
@@ -1,11 +1,58 @@
 package com.growthook.aos.presentation.insight.noactionplan
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.growthook.aos.domain.entity.SeedInfo
+import com.growthook.aos.util.extension.addSourceList
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class SeedModifyViewModel: ViewModel() {
+@HiltViewModel
+class SeedModifyViewModel @Inject constructor(): ViewModel() {
+
+    private val _insightModify: MutableLiveData<String> = MutableLiveData()
+    val insightModify: LiveData<String>
+        get() = _insightModify
+
+    private val _memoModify: MutableLiveData<String> = MutableLiveData()
+    val memoModify: LiveData<String>
+        get() = _memoModify
+
+    private val _sourceModify: MutableLiveData<String> = MutableLiveData()
+    val sourceModify: LiveData<String>
+        get() = _sourceModify
+
+    private val _urlModify: MutableLiveData<String> = MutableLiveData()
+    val urlModify: LiveData<String>
+        get() = _urlModify
+
+    fun setInsightModify(insight: String) {
+        _insightModify.value = insight
+    }
+
+    fun setMemoModify(memo: String) {
+        _memoModify.value = memo
+    }
+
+    fun setSourceModify(source: String) {
+        _sourceModify.value = source
+    }
+
+    fun setUrlModify(url: String) {
+        _urlModify.value = url
+    }
+
+    val checkSeedModifyBtnEnabled = MediatorLiveData<Boolean>().apply {
+        addSourceList(insightModify, memoModify, sourceModify, urlModify) { checkSeedModifyEnabled() }
+    }
+
+    private fun checkSeedModifyEnabled(): Boolean =
+        insightModify.value != _seedInfo.value?.insight
+                || memoModify.value != _seedInfo.value?.memo
+                || sourceModify.value != _seedInfo.value?.source
+                || urlModify.value != _seedInfo.value?.url
 
     private val _seedInfo: MutableLiveData<SeedInfo> = MutableLiveData(
        SeedInfo(
