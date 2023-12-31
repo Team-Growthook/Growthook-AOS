3f436103df34292dd1d2cbb0b612c2be9b427974
gaeun <geun5744@gmail.com>
2023-12-31 17:20:49 +0900

    [ADD/#36] 씨앗 옮기기 Service 추가

--- app/src/main/java/com/growthook/aos/data/service/SeedService.kt
+++ app/src/main/java/com/growthook/aos/data/service/SeedService.kt
@@ -1,7 +1,11 @@
 package com.growthook.aos.data.service
 
+import com.growthook.aos.data.model.request.RequestSeedMoveDto
 import com.growthook.aos.data.model.response.ResponseDto
+import com.growthook.aos.data.model.response.ResponseMoveSeedDto
+import retrofit2.http.Body
 import retrofit2.http.DELETE
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface SeedService {
@@ -10,4 +14,10 @@ interface SeedService {
     suspend fun deleteSeed(
         @Path("seedId") seedId: Int,
     ): ResponseDto
+
+    @POST("api/v1/seed/{seedId}/move")
+    suspend fun moveSeed(
+        @Path("seedId") seedId: Int,
+        @Body request: RequestSeedMoveDto,
+    ): ResponseMoveSeedDto
 }
