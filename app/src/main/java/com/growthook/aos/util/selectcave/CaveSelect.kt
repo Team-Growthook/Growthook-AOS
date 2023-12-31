5b4f1ba3ca3cd0db6b6577920c861afb5c1ad9ed
gaeun <geun5744@gmail.com>
2024-01-01 02:30:06 +0900

    [FEAT/#36] type에 따른 bottomSheet 구현

--- app/src/main/java/com/growthook/aos/util/selectcave/CaveSelect.kt
+++ app/src/main/java/com/growthook/aos/util/selectcave/CaveSelect.kt
@@ -1,17 +1,17 @@
 package com.growthook.aos.util.selectcave
 
-import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import com.growthook.aos.R
 import com.growthook.aos.databinding.FragmentCaveSelectBottomsheetBinding
+import com.growthook.aos.domain.entity.Cave
 import com.growthook.aos.util.base.BaseBottomSheetFragment
 
 abstract class CaveSelect :
     BaseBottomSheetFragment<FragmentCaveSelectBottomsheetBinding>(R.layout.fragment_cave_select_bottomsheet) {
 
     protected var toMoveSeedId: Int = 0
+    protected lateinit var clickBtnAction: (Cave) -> Unit
 
     override fun getFragmentBinding(
         inflater: LayoutInflater,
@@ -19,18 +19,27 @@ abstract class CaveSelect :
     ): FragmentCaveSelectBottomsheetBinding =
         FragmentCaveSelectBottomsheetBinding.inflate(inflater, container, false)
 
-    abstract fun moveSeed()
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        moveSeed()
-    }
-
     class Builder {
-        fun build(toMoveSeedId: Int): CaveSelect {
-            return CaveSelectBottomSheet().apply {
+        fun build(
+            type: CaveSelectType,
+            toMoveSeedId: Int,
+            clickBtnAction: (Cave) -> Unit = {},
+        ): CaveSelect {
+            return type.getInstance().apply {
                 this.toMoveSeedId = toMoveSeedId
+                this.clickBtnAction = clickBtnAction
             }
         }
+    }
+
+    enum class CaveSelectType {
+        YES_API {
+            override fun getInstance(): CaveSelect = YesApiCaveSelectBottomSheet()
+        },
+        NO_API {
+            override fun getInstance(): CaveSelect = NoApiCaveSelectBottomSheet()
+        }, ;
+
+        abstract fun getInstance(): CaveSelect
     }
 }
