4348843d40e291e3132db6d01d15c53b200df045
KIM MINJEONG <minjeong030420@gmail.com>
2024-01-02 03:28:47 +0900

    [ADD/#38] service & dataSource 생성

--- app/src/main/java/com/growthook/aos/data/service/CaveService.kt
+++ app/src/main/java/com/growthook/aos/data/service/CaveService.kt
@@ -1,13 +1,16 @@
 package com.growthook.aos.data.service
 
 import com.growthook.aos.data.model.request.RequestCaveModifyDto
+import com.growthook.aos.data.model.request.RequestCavePostDto
 import com.growthook.aos.data.model.response.ResponseDto
 import com.growthook.aos.data.model.response.ResponseGetCavesDto
 import com.growthook.aos.data.model.response.ResponseGetDetailCaveDto
+import com.growthook.aos.data.model.response.ResponsePostCaveDto
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.PATCH
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface CaveService {
@@ -33,4 +36,10 @@ interface CaveService {
         @Path("caveId") caveId: Int,
         @Body request: RequestCaveModifyDto,
     ): ResponseDto
+
+    @POST("/api/v1/member/{memberId}/cave")
+    suspend fun postCave(
+        @Path("memberId") memberId: Int,
+        @Body request: RequestCavePostDto
+    ): ResponsePostCaveDto
 }
