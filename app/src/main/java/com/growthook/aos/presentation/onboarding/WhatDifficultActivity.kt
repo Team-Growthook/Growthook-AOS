3a1ff7095146b975b1fcb6353662ced6b490f65e
gaeun <geun5744@gmail.com>
2024-01-03 01:30:23 +0900

    [UI/#34] 온보딩 step1 UI 구현

---
+++ app/src/main/java/com/growthook/aos/presentation/onboarding/WhatDifficultActivity.kt
@@ -0,0 +1,7 @@
+package com.growthook.aos.presentation.onboarding
+
+import com.growthook.aos.databinding.ActivityWhatDifficultBinding
+import com.growthook.aos.util.base.BaseActivity
+
+class WhatDifficultActivity :
+    BaseActivity<ActivityWhatDifficultBinding>({ ActivityWhatDifficultBinding.inflate(it) })
