38ccbee4fddc7e607dd56dccee732d7b79fbb077
gaeun <geun5744@gmail.com>
2024-01-03 17:45:11 +0900

    [ADD/#34] SolutionActivity 추가

---
+++ app/src/main/java/com/growthook/aos/presentation/onboarding/SolutionActivity.kt
@@ -0,0 +1,14 @@
+package com.growthook.aos.presentation.onboarding
+
+import android.os.Bundle
+import com.growthook.aos.databinding.ActivitySolutionBinding
+import com.growthook.aos.util.base.BaseActivity
+
+class SolutionActivity : BaseActivity<ActivitySolutionBinding>({
+    ActivitySolutionBinding.inflate(it)
+}) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
