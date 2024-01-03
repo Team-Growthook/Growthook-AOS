8092f21d13db7f586be80df313ab6d9923ea5910
KIM MINJEONG <minjeong030420@gmail.com>
2024-01-04 02:25:51 +0900

    [ADD/#44] service & dataSource 생성

--- app/src/main/java/com/growthook/aos/data/datasource/remote/SeedDataSource.kt
+++ app/src/main/java/com/growthook/aos/data/datasource/remote/SeedDataSource.kt
@@ -1,11 +1,15 @@
 package com.growthook.aos.data.datasource.remote
 
 import com.growthook.aos.data.model.request.RequestSeedMoveDto
+import com.growthook.aos.data.model.request.RequestSeedPostDto
 import com.growthook.aos.data.model.response.ResponseDto
 import com.growthook.aos.data.model.response.ResponseMoveSeedDto
+import com.growthook.aos.data.model.response.ResponsePostSeedDto
 
 interface SeedDataSource {
     suspend fun deleteSeed(seedId: Int): ResponseDto
 
     suspend fun moveSeed(seedId: Int, request: RequestSeedMoveDto): ResponseMoveSeedDto
+
+    suspend fun postSeed(caveId: Int, request: RequestSeedPostDto): ResponsePostSeedDto
 }
--- app/src/main/java/com/growthook/aos/data/datasource/remote/impl/SeedDataSourceImpl.kt
+++ app/src/main/java/com/growthook/aos/data/datasource/remote/impl/SeedDataSourceImpl.kt
@@ -2,8 +2,10 @@ package com.growthook.aos.data.datasource.remote.impl
 
 import com.growthook.aos.data.datasource.remote.SeedDataSource
 import com.growthook.aos.data.model.request.RequestSeedMoveDto
+import com.growthook.aos.data.model.request.RequestSeedPostDto
 import com.growthook.aos.data.model.response.ResponseDto
 import com.growthook.aos.data.model.response.ResponseMoveSeedDto
+import com.growthook.aos.data.model.response.ResponsePostSeedDto
 import com.growthook.aos.data.service.SeedService
 import javax.inject.Inject
 
@@ -11,4 +13,7 @@ class SeedDataSourceImpl @Inject constructor(private val apiService: SeedService
     override suspend fun deleteSeed(seedId: Int): ResponseDto = apiService.deleteSeed(seedId)
     override suspend fun moveSeed(seedId: Int, request: RequestSeedMoveDto): ResponseMoveSeedDto =
         apiService.moveSeed(seedId, request)
+
+    override suspend fun postSeed(caveId: Int, request: RequestSeedPostDto): ResponsePostSeedDto =
+        apiService.postSeed(caveId, request)
 }
