a3cc3e2cccc743fcef34c0ffa194bb010207c243
Cat-JiYoung <kimyamae82@gmail.com>
2024-01-11 03:35:37 +0900

    [FEAT/#40] 액션플랜 생성 dataSource 추가

--- app/src/main/java/com/growthook/aos/data/datasource/remote/impl/ActionplanDataSourceImpl.kt
+++ app/src/main/java/com/growthook/aos/data/datasource/remote/impl/ActionplanDataSourceImpl.kt
@@ -1,6 +1,8 @@
 package com.growthook.aos.data.datasource.remote.impl
 
 import com.growthook.aos.data.datasource.remote.ActionplanDataSource
+import com.growthook.aos.data.model.request.RequestActionplanPostDto
+import com.growthook.aos.data.model.response.ResponseDto
 import com.growthook.aos.data.model.response.ResponseGetActionplanDto
 import com.growthook.aos.data.service.ActionplanService
 import javax.inject.Inject
@@ -9,4 +11,10 @@ class ActionplanDataSourceImpl @Inject constructor(private val apiService: Actio
     ActionplanDataSource {
     override suspend fun getActionplans(seedId: Int): ResponseGetActionplanDto =
         apiService.getActionplans(seedId)
+
+    override suspend fun postActionplans(
+        seedId: Int,
+        request: RequestActionplanPostDto,
+    ): ResponseDto =
+        apiService.postActionplans(seedId, request)
 }
