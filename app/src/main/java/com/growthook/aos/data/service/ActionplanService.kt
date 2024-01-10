2721be488fcaa4e9b3356cf43e95b3c3674508b6
Cat-JiYoung <kimyamae82@gmail.com>
2024-01-11 03:35:23 +0900

    [FEAT/#40] 액션플랜 생성 dto 추가

--- app/src/main/java/com/growthook/aos/data/service/ActionplanService.kt
+++ app/src/main/java/com/growthook/aos/data/service/ActionplanService.kt
@@ -1,7 +1,11 @@
 package com.growthook.aos.data.service
 
+import com.growthook.aos.data.model.request.RequestActionplanPostDto
+import com.growthook.aos.data.model.response.ResponseDto
 import com.growthook.aos.data.model.response.ResponseGetActionplanDto
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface ActionplanService {
@@ -9,4 +13,10 @@ interface ActionplanService {
     suspend fun getActionplans(
         @Path("seedId") seedId: Int,
     ): ResponseGetActionplanDto
+
+    @POST("api/v1/seed/{seedId}/actionplan")
+    suspend fun postActionplans(
+        @Path("seedId") seedId: Int,
+        @Body request: RequestActionplanPostDto,
+    ): ResponseDto
 }
