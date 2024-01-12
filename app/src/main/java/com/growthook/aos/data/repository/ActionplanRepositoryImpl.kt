b40e39af24730d0bdc38c0ba0fbf4405ca8407e2
Cat-JiYoung <kimyamae82@gmail.com>
2024-01-12 18:53:13 +0900

    [FEAT/#51] 액션리스트 조회 repository 추가

--- app/src/main/java/com/growthook/aos/data/repository/ActionplanRepositoryImpl.kt
+++ app/src/main/java/com/growthook/aos/data/repository/ActionplanRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.growthook.aos.data.repository
 
 import com.growthook.aos.data.datasource.remote.ActionplanDataSource
 import com.growthook.aos.data.model.request.RequestActionplanPostDto
+import com.growthook.aos.domain.entity.ActionlistDetail
 import com.growthook.aos.domain.entity.Actionplan
 import com.growthook.aos.domain.repository.ActionplanRepository
 import javax.inject.Inject
@@ -15,5 +16,15 @@ class ActionplanRepositoryImpl @Inject constructor(private val actionplanDataSou
     override suspend fun postActionplans(seedId: Int, contents: List<String>): Result<Unit> =
         runCatching {
             actionplanDataSource.postActionplans(seedId, RequestActionplanPostDto(contents))
+        }
+
+    override suspend fun getDoingActionplans(memberId: Int): Result<List<ActionlistDetail>> =
+        runCatching {
+            actionplanDataSource.getDoingActionplans(memberId).toActionlist()
+        }
+
+    override suspend fun getFinishedActionplans(memberId: Int): Result<List<ActionlistDetail>> =
+        runCatching {
+            actionplanDataSource.getFinishedActionplans(memberId).toActionlist()
         }
 }
--- app/src/main/java/com/growthook/aos/domain/repository/ActionplanRepository.kt
+++ app/src/main/java/com/growthook/aos/domain/repository/ActionplanRepository.kt
@@ -1,9 +1,14 @@
 package com.growthook.aos.domain.repository
 
+import com.growthook.aos.domain.entity.ActionlistDetail
 import com.growthook.aos.domain.entity.Actionplan
 
 interface ActionplanRepository {
     suspend fun getActionplans(seedId: Int): Result<List<Actionplan>>
 
     suspend fun postActionplans(seedId: Int, contents: List<String>): Result<Unit>
+
+    suspend fun getDoingActionplans(memberId: Int): Result<List<ActionlistDetail>>
+
+    suspend fun getFinishedActionplans(memberId: Int): Result<List<ActionlistDetail>>
 }
