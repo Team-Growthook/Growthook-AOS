3f1a1f0f62d5a5842a0914c9b67ab6c6b9029b7a
Cat-JiYoung <kimyamae82@gmail.com>
2024-01-14 01:00:00 +0900

    [FEAT/#51] review 상세조회 dataSource 추가

--- app/src/main/java/com/growthook/aos/data/datasource/remote/ReviewDataSource.kt
+++ app/src/main/java/com/growthook/aos/data/datasource/remote/ReviewDataSource.kt
@@ -1,7 +1,7 @@
 package com.growthook.aos.data.datasource.remote
 
-import com.growthook.aos.data.model.response.ResponseDto
+import com.growthook.aos.data.model.response.ResponseGetReviewDto
 
 interface ReviewDataSource {
-    suspend fun getReviewDetail(actionplanId: Int): ResponseDto
+    suspend fun getReviewDetail(actionplanId: Int): ResponseGetReviewDto
 }
--- app/src/main/java/com/growthook/aos/data/datasource/remote/impl/ReviewDataSourceImpl.kt
+++ app/src/main/java/com/growthook/aos/data/datasource/remote/impl/ReviewDataSourceImpl.kt
@@ -1,12 +1,12 @@
 package com.growthook.aos.data.datasource.remote.impl
 
 import com.growthook.aos.data.datasource.remote.ReviewDataSource
-import com.growthook.aos.data.model.response.ResponseDto
+import com.growthook.aos.data.model.response.ResponseGetReviewDto
 import com.growthook.aos.data.service.ReviewService
 import javax.inject.Inject
 
 class ReviewDataSourceImpl @Inject constructor(private val apiService: ReviewService) :
     ReviewDataSource {
-    override suspend fun getReviewDetail(actionplanId: Int): ResponseDto =
+    override suspend fun getReviewDetail(actionplanId: Int): ResponseGetReviewDto =
         apiService.getReviewDetail(actionplanId)
 }
