d42c9e76e471309a19ea175e80299a6c06857ea2
Cat-JiYoung <kimyamae82@gmail.com>
2024-01-14 01:07:14 +0900

    [FEAT/#51] review 생성 api 함수 추가

--- app/src/main/java/com/growthook/aos/data/service/ReviewService.kt
+++ app/src/main/java/com/growthook/aos/data/service/ReviewService.kt
@@ -1,7 +1,11 @@
 package com.growthook.aos.data.service
 
+import com.growthook.aos.data.model.request.RequestReviewDto
+import com.growthook.aos.data.model.response.ResponseDto
 import com.growthook.aos.data.model.response.ResponseGetReviewDto
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface ReviewService {
@@ -9,4 +13,10 @@ interface ReviewService {
     suspend fun getReviewDetail(
         @Path("actionplanId") actionplanId: Int,
     ): ResponseGetReviewDto
+
+    @POST("api/v1/actionplan/{actionplanId}/review")
+    suspend fun postReview(
+        @Path("actionplanId") actionplanId: Int,
+        @Body request: RequestReviewDto,
+    ): ResponseDto
 }
