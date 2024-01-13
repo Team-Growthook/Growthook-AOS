39da044571e4954845fb534d7d8c0b9a2e9d6998
Cat-JiYoung <kimyamae82@gmail.com>
2024-01-14 01:07:37 +0900

    [FEAT/#51] review 생성 dataSource 추가

--- app/src/main/java/com/growthook/aos/data/datasource/remote/ReviewDataSource.kt
+++ app/src/main/java/com/growthook/aos/data/datasource/remote/ReviewDataSource.kt
@@ -1,7 +1,11 @@
 package com.growthook.aos.data.datasource.remote
 
+import com.growthook.aos.data.model.request.RequestReviewDto
+import com.growthook.aos.data.model.response.ResponseDto
 import com.growthook.aos.data.model.response.ResponseGetReviewDto
 
 interface ReviewDataSource {
     suspend fun getReviewDetail(actionplanId: Int): ResponseGetReviewDto
+
+    suspend fun postReview(actionplanId: Int, request: RequestReviewDto): ResponseDto
 }
--- app/src/main/java/com/growthook/aos/data/datasource/remote/impl/ReviewDataSourceImpl.kt
+++ app/src/main/java/com/growthook/aos/data/datasource/remote/impl/ReviewDataSourceImpl.kt
@@ -1,6 +1,8 @@
 package com.growthook.aos.data.datasource.remote.impl
 
 import com.growthook.aos.data.datasource.remote.ReviewDataSource
+import com.growthook.aos.data.model.request.RequestReviewDto
+import com.growthook.aos.data.model.response.ResponseDto
 import com.growthook.aos.data.model.response.ResponseGetReviewDto
 import com.growthook.aos.data.service.ReviewService
 import javax.inject.Inject
@@ -9,4 +11,7 @@ class ReviewDataSourceImpl @Inject constructor(private val apiService: ReviewSer
     ReviewDataSource {
     override suspend fun getReviewDetail(actionplanId: Int): ResponseGetReviewDto =
         apiService.getReviewDetail(actionplanId)
+
+    override suspend fun postReview(actionplanId: Int, request: RequestReviewDto): ResponseDto =
+        apiService.postReview(actionplanId, request)
 }
