2b17608864a11253050ceb4de2fb7f8f277f81c9
gaeun <geun5744@gmail.com>
2024-01-20 22:10:03 +0900

    [ADD/#59] 회원가입 annotation 추가

--- app/src/main/java/com/growthook/aos/di/Qualifier.kt
+++ app/src/main/java/com/growthook/aos/di/Qualifier.kt
@@ -5,3 +5,7 @@ import javax.inject.Qualifier
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
 annotation class GrowthookRetrofit
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class SignUpRetrofit
