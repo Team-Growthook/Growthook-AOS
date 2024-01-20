7f1127b0cb421d45f48613fd5ff508a0c582f85a
gaeun <geun5744@gmail.com>
2024-01-20 23:10:26 +0900

    [FEAT/#59] SignUpRetrofit 생성

---
+++ app/src/main/java/com/growthook/aos/di/SignUpRetrofitModule.kt
@@ -0,0 +1,59 @@
+package com.growthook.aos.di
+
+import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.growthook.aos.BuildConfig
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SignUpRetrofitModule {
+    private const val PUZZLING_BASE_URL = BuildConfig.GROWTHOOK_BASE_URL
+
+    @Provides
+    @Singleton
+    @SignUpRetrofit
+    fun provideLoginOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
+        @SignUpRetrofit tokenInterceptor: Interceptor,
+        networkFlipperPlugin: NetworkFlipperPlugin,
+    ): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor(tokenInterceptor)
+            .addNetworkInterceptor(FlipperOkhttpInterceptor(networkFlipperPlugin))
+            .build()
+
+    @Singleton
+    @Provides
+    @SignUpRetrofit
+    fun provideLoginRetrofit(@SignUpRetrofit okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .baseUrl(PUZZLING_BASE_URL)
+            .client(okHttpClient)
+            .build()
+
+    @Singleton
+    @Provides
+    @SignUpRetrofit
+    fun loginInterceptor(): Interceptor {
+        val requestInterceptor = Interceptor { chain ->
+            val original = chain.request()
+            val builder = original.newBuilder()
+            chain.proceed(builder.build())
+        }
+        return requestInterceptor
+    }
+}
