7bbd055ff0923eb42c62dec2b2b93f05eb73c6bf
gaeun <geun5744@gmail.com>
2024-01-22 17:32:09 +0900

    [FEAT/#59] 토큰 재발급시, local에 반환값 저장 구현

--- app/src/main/java/com/growthook/aos/data/repository/RefreshRepository.kt
+++ app/src/main/java/com/growthook/aos/data/repository/RefreshRepository.kt
@@ -3,11 +3,17 @@ package com.growthook.aos.data.repository
 import com.growthook.aos.data.datasource.remote.RefreshDataSource
 import com.growthook.aos.data.entity.Token
 import com.growthook.aos.domain.repository.RefreshRepository
+import com.growthook.aos.domain.repository.TokenRepository
 import javax.inject.Inject
 
-class RefreshRepository @Inject constructor(private val dataSource: RefreshDataSource) :
+class RefreshRepository @Inject constructor(
+    private val dataSource: RefreshDataSource,
+    private val tokenRepository: TokenRepository,
+) :
     RefreshRepository {
     override suspend fun getRefreshToken(): Result<Token> = runCatching {
-        dataSource.getRefreshToken().toRefresh()
+        val token = dataSource.getRefreshToken().toRefresh()
+        tokenRepository.setToken(token.accessToken ?: "", token.refreshToken ?: "")
+        token
     }
 }
