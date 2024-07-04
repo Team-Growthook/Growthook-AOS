6aced1018df747306b2003cb90b1d3ef4bd17ec1
Cat-JiYoung <kimyamae82@gmail.com>
2024-07-04 17:10:48 +0900

    [FEAT/#133] 네비게이션을 위한 더미 스크린 파일 추가

---
+++ app/src/main/java/com/growthook/aos/presentation/todolist/TodoScreen.kt
@@ -0,0 +1,22 @@
+package com.growthook.aos.presentation.todolist
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TodoScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "TodoScreen")
+    }
+}
